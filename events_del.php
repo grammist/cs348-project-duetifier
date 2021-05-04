d87bfa9ad5fdb68ccfac6c5b6195478e5d2d72da
grammist <75149833+grammist@users.noreply.github.com>
2021-05-04 00:05:59 -0400

    Update events_del.php

--- events_del.php
+++ events_del.php
@@ -2,6 +2,7 @@
 require 'assets/functions/functions.php';
 $id = $_GET['id'];
 
+mysqli_query($connection, "SET SESSION TRANSACTION ISOLATION LEVEL SERIALIZABLE");//JBZ
 // stored procedures 
 $conection->query("call delEvent('" . $id . "')");
 
